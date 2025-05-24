b37c0cd85b84af9332aefd9e75de4a829f50ce7f
pritamsap <pritamsap2@gmail.com>
2025-05-24 15:09:07 -0400

    Initial Commit

---
+++ FridgeExpiryTracker/FridgeExpiryTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FridgeExpiryTrackerApp.swift
+//  FridgeExpiryTracker
+//
+//  Created by pritam on 2025-05-24.
+//
+
+import SwiftUI
+
+@main
+struct FridgeExpiryTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FridgeExpiryTrackerTests/FridgeExpiryTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  FridgeExpiryTrackerTests.swift
+//  FridgeExpiryTrackerTests
+//
+//  Created by pritam on 2025-05-24.
+//
+
+import Testing
+@testable import FridgeExpiryTracker
+
+struct FridgeExpiryTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
